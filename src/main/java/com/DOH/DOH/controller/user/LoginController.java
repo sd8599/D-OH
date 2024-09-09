f6a32ec6327f133e0fdda07c6b91076ffd41cff2
mkh4014 <167828774+mkh4014@users.noreply.github.com>
2024-09-09 14:24:08 +0900

    로그인 페이지 화면 작업
    - html과 css link로 연결
    - 로그인, 회원가입 기능을 위한 LoginDTO 클래스 추가
    - 로그인 페이지 이동을 위한 Logincontroller 작성

--- src/main/java/com/DOH/DOH/controller/user/LoginController.java
+++ src/main/java/com/DOH/DOH/controller/user/LoginController.java
@@ -2,8 +2,22 @@ package com.DOH.DOH.controller.user;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class LoginController {
+    //메인 로그인 페이지 로드
+    @GetMapping("/loginMain")
+    public String showLoginMainPage() {
+
+        return "user/loginMain";
+    }
+    @GetMapping("/login")
+    public String showLoginPage() {
+
+        return "user/login";
+    }
+
 }
